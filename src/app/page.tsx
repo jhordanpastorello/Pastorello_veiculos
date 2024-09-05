bc1821dbf1e1d249a3f88884aeec05c87c7d697b
Jhordan Pastorello <jhordanpastorello@MacBook-Pro-de-Jhordan.local>
2024-09-05 20:00:59 -0300

    Layout Smatphone 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,16 @@ export default function Home() {
     
     <main className="flex h-screen w-screen items-center justify-center bg-[url('/images/bg.png')] bg-cover ">
       <NavBar/>
-      <div className="fixed right-0 w-5/6 h-screen flex flex-col justify-start items-end ">
-        <div className=" m-8 sm:w-4/6 md:w-3/6 lg:w-2/6  sm:h-4/6 lg:h-2/6 bg-zinc-700/60 rounded-2xl flex flex-col justify-center items-center text-center text-white">
-          <h1 className="font-black sm:text-xl md:text-2xl">QUERENDO TROCAR <br/> DE CARRO?</h1>
-          <p className="sm:font-light md:font-semibold mt-4">
+      <div className="fixed right-0 w-5/6 h-screen flex flex-col justify-start items-end max-sm:justify-center max-sm:items-center ">
+        <div className=" m-8 max-sm:w-5/6 md:w-3/6 lg:w-3/6  masx-sm:h-5/6 lg:h-2/6 bg-zinc-700/60 rounded-2xl flex flex-col justify-center items-center text-center text-white">
+          <h1 className="font-black max-sm:text-2xl ">QUERENDO TROCAR <br/> DE CARRO?</h1>
+          <p className="max-sm:font-thin mt-6 m-4">
             Aqui é o melhor lugar, aproveite!!<br/>
             São ofertas espetaculares e recheadas de beneficios <br/>
             esperando apenas por você, não perca tempo !!!<br/>
             clique em saiba mais, e conheça nossas ofertas.
           </p>
-          <button className="animate-bounce text-zinc-700 bg-yellow-400 mt-8 w-5/6 h-1/5 rounded-3xl font-black text-xl">
+          <button className="animate-bounce text-zinc-700 bg-yellow-400 mt-4 w-5/6 h-1/5 rounded-3xl font-black text-xl">
             <a href="https://wa.me/46999019800"> Saiba Mais!</a>
           </button>
 
